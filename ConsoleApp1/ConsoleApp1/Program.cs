7867b96b55d9bc1bd1dd2f115d69f3fc5ba68bc2
NargizOM <nargizom@code.edu.az>
2018-02-23 20:31:15 +0400

    console

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            { Console.WriteLine(i + " eded konfet verildi");
+            }
+
+            {
+                for (int i = 1; i<5; i++)
+                {
+                    Console.WriteLine(i+ 2);
+                }
+
+            }
+        }
+               
+	
+            }
+
+}
